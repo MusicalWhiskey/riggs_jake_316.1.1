f40adae8f31d2f108124d8cd0632066ec98d9e45
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-09-27 00:05:38 -0700

    navbar links

--- src/script.js
+++ src/script.js
@@ -18,3 +18,20 @@ topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--top-menu-bg');
 
 topMenuEl.classList.add('flex-around');
+
+const menuLinks = [
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '/catalog' },
+  { text: 'orders', href: '/orders' },
+  { text: 'account', href: '/account' },
+];
+
+menuLinks.forEach(function(link) {
+  var linkElement = document.createElement('a');
+  
+  linkElement.setAttribute('href', link.href);
+  
+  linkElement.textContent = link.text;
+  
+  topMenuEl.appendChild(linkElement);
+});

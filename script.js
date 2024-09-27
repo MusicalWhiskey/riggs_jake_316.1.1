33d4d420b9a96abb6eb8360abf8e9883491439b3
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-09-26 22:39:17 -0700

    still trying to get style sheet to work

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+// import = "./style.css";
+
 console.log('316.1.1'),
 //=======================================
+
+mainEl = document.querySelector('main');
+
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+
+mainEl.classList.add('flex-ctr');

0105c1853d5a59786a4bcf706c43aaa6ed5175c0
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-09-26 22:57:21 -0700

    navbar

--- src/script.js
+++ src/script.js
@@ -10,3 +10,11 @@ mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 
 mainEl.classList.add('flex-ctr');
+
+const topMenuEl = document.querySelector('#top-menu');
+
+topMenuEl.style.height = '100%';
+
+topMenuEl.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--top-menu-bg');
+
+topMenuEl.classList.add('flex-around');
